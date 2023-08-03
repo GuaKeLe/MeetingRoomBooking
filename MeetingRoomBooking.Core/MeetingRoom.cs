dac5c5b6551f4db7a35590d15d8bdf61d6c67f10
Rudy Zeng <guakele121@gmail.com>
2023-08-03 08:04:42 +0800

    InitCreation

---
+++ MeetingRoomBooking.Core/MeetingRoom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeetingRoomBooking.Core.Supporting;
+
+namespace MeetingRoomBooking.Core
+{
+    public class MeetingRoom
+    {
+        public string Id { get;set; } = Guid.NewGuid().ToString();
+        public string? Name { get; set; }
+        public int Floor { get; set; }
+        public int Capacity { get; set; }
+        public IEnumerable<RoomImage>? Images { get; set; }
+
+        public bool IsVideoSupportOrNot { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool DescriptionIfNotAvailable { get; set; }
+    }
+}
---
+++ MeetingRoomBooking.Core/Supporting/RoomImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingRoomBooking.Core.Supporting
+{
+    public class RoomImage
+    {
+        public string? ImagePath { get; set; }
+        public string Name { get; set; } = "Image - ";
+        public MeetingRoom? UsingMeetingRoom { get; set; }
+    }
+}
